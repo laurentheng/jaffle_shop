ac4136395fafcadaa6685904b9b0e597267bb130
laurent.heng@gmail.com <laurent.heng@gmail.com>
2024-09-10 20:58:21 +0000

    creation of unit_tests.yml + modification of stg_jaffle_shop__orders consequently

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -12,7 +12,14 @@ renamed as (
         id AS order_id,
         user_id AS customer_id,
         order_date,
-        status,
+        DATE_DIFF({{ dbt.current_timestamp() }}, CAST(order_date AS TIMESTAMP), DAY) AS days_since_ordered,
+        status LIKE '%pending%' AS is_status_pending,
+        CASE
+            WHEN status LIKE '%shipped%' THEN 'shipped'
+            WHEN status LIKE '%return%' THEN 'returned'
+            WHEN status LIKE '%pending%' THEN 'placed'
+            ELSE status
+        END AS status,
         _etl_loaded_at AS etl_loaded_at
 
     from source
